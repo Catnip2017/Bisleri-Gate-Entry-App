05c776fc3abe58f0f1a5809bf7e50a0bdf061a2a
Jat Shah <jat.shah@bisleri.com>
2025-09-11 13:32:24 +0530

    main changes

--- bisleri-backend/app/main.py
+++ bisleri-backend/app/main.py
@@ -17,6 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "Http://localhost:8000"
         "http://localhost:8081",
         "http://127.0.0.1:8081", 
         "http://192.168.1.10:8081",
