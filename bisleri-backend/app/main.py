f44b2225450185d823bbeb73db1dbaa31d4478b7
vignesh <vingesh820@gmail.com>
2025-09-24 11:35:54 +0530

    Apply stashed changes and resolve conflicts

--- bisleri-backend/app/main.py
+++ bisleri-backend/app/main.py
@@ -40,7 +40,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8081",
         "http://127.0.0.1:8081", 
+
+        "http://10.148.248.117:8081",
+        
         "http://192.168.1.2:8081",
+
         "http://123.63.20.237:8081",
         "http://192.168.1.56:8081",
         # Add nginx proxy URLs
